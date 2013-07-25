c4fae544d8b3c52cba7aa2718ed0e0505a56629e
n0-tactic <stimmeinferne@mail.ru>
2013-07-26 00:17:03 +0900

    my first app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Guestbook::Application.config.secret_token = '0be7773d81505c7e19ab4d55894c746608b5049bdda28019681a252e8df4fe774838380e2db9a13c024de8bab8a6ff0453e92ab2c1ccb47b9ebaf8284c8ba4e1'
